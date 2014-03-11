4053fda5f430b552b94e8e383b7199894f130796
Dan Ubilla <dan.ubilla@gmail.com>
2014-03-10 21:16:59 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Augusta::Application.config.secret_token = '48729b1ff042cf0a5d19601c26704a5ee3d7954903217583c9f690de6b42ed53d8931886334f2895a90eab800e19adcecda1d0ad81905165706f320eab5549ff'
