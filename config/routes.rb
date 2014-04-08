cc12c43a34c4d8e8f2f46a8a1dd4951d73f86d52
Dan Ubilla <dan.ubilla@gmail.com>
2014-04-07 23:57:01 -0400

    Add teams

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Augusta::Application.routes.draw do
+  resources :teams
+
   devise_for :users
+
+  root to: "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
