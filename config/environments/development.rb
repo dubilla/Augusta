b2c644423aaaf911a346f520db12c72796be5e96
Dan Ubilla <dan.ubilla@gmail.com>
2014-04-07 23:24:27 -0400

    Add users with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Augusta::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
