7fa799d91f557303e147dd6c6c41ae358ed031d0
Dan Ubilla <dan.ubilla@gmail.com>
2014-04-20 10:53:00 -0400

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 20140410042549) do
   end
 
   create_table "teams", force: true do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
   end
 
@@ -41,8 +41,8 @@ ActiveRecord::Schema.define(version: 20140410042549) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
