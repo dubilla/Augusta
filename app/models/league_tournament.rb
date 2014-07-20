e8fc6521e72d2f75e7189fa2f330cb48d9b84e41
Dan Ubilla <dan.ubilla@gmail.com>
2014-07-20 15:29:33 -0400

    Fix sort by on tournaments on league page

--- app/models/league_tournament.rb
+++ app/models/league_tournament.rb
@@ -5,6 +5,7 @@ class LeagueTournament < ActiveRecord::Base
   has_many :rosters
 
   delegate :external_id, to: :tournament
+  delegate :start_date, to: :tournament
 
   def roster_players
     rosters.collect(&:roster_players).flatten
