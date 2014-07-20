0570f563313423ddef880de3d00e29203a6cf1f2
Dan Ubilla <dan.ubilla@gmail.com>
2014-07-20 13:06:54 -0400

    Add team leaderboard

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -3,7 +3,9 @@ class LeaderboardsController < ApplicationController
   def index
     @league_tournament = LeagueTournament.find(params[:league_tournament_id])
 
-    @player_leaders = @league_tournament.roster_players.sort_by{|r| r.score }
+    @player_leaders = @league_tournament.roster_players.sort_by{ |p| p.score }
+
+    @team_leaders = @league_tournament.rosters.sort_by{ |r| r.score }
 
     respond_to do |format|
       format.html
--- app/models/roster.rb
+++ app/models/roster.rb
@@ -5,4 +5,18 @@ class Roster < ActiveRecord::Base
   belongs_to :team
   belongs_to :league_tournament
 
+  def score
+    roster_players.map{|p| p.score}.reduce(:+)
+  end
+
+  def tournament_athletes
+    tournament_golfers.detect{ |g| g["athlete"]["id"] == external_id }
+  end
+
+  private
+
+  def tournament_golfers
+    TournamentData.new(league_tournament.external_id).athletes
+  end
+
 end
