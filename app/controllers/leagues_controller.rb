cfc271e843a39f5c914aa2464f0becc2e21223fd
Dan Ubilla <dan.ubilla@gmail.com>
2014-07-20 15:45:38 -0400

    Add list of leagues

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,5 +1,13 @@
 class LeaguesController < ApplicationController
 
+  def index
+    @leagues = League.all
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
   def show
     @league = League.find(params[:id])
 
