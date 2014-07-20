b5b6ff0c45b96262cf99acfa5bf674ba4a3b9ede
Dan Ubilla <dan.ubilla@gmail.com>
2014-07-20 12:52:25 -0400

    Remove extraneous code

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -2,12 +2,6 @@ class LeaderboardsController < ApplicationController
 
   def index
     @league_tournament = LeagueTournament.find(params[:league_tournament_id])
-    @tournament ||= TournamentData.new(@league_tournament.external_id).event
-    league_tournament_players = @league_tournament.rosters.collect(&:players).flatten
-    player_ids = league_tournament_players.collect(&:external_id)
-
-    all_competitors = @tournament["competitors"]
-    @player_leaders = all_competitors.select{|g| player_ids.include? g["athlete"]["id"] }
 
     @player_leaders = @league_tournament.roster_players.sort_by{|r| r.score }
 
