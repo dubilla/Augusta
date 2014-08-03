ae340e09ae04a0003e12ec64f13769a982826bcf
Dan Ubilla <dan.ubilla@gmail.com>
2014-08-02 22:00:18 -0400

    Add breadcrumbs

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,7 +1,7 @@
 class LeaderboardsController < ApplicationController
 
   def index
-    @league_tournament = LeagueTournament.find(params[:league_tournament_id])
+    @league_tournament = LeagueTournament.includes(:league).find(params[:league_tournament_id])
 
     @player_leaders = @league_tournament.roster_players.map{ |p| p.decorate.to_json }.sort_by{ |p| p[:score] }
 
