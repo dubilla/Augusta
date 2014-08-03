daa52d8055fccec506e772c54daa8cbe92cf7d86
Dan Ubilla <dan.ubilla@gmail.com>
2014-08-02 21:23:33 -0400

    Fix sign in buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for resource
-    team_path current_user.team || root_path
+    league_team_path(current_user.team.league, current_user.team) || root_path
   end
 end
