51138460ecdb8dcb135de3220e2f87f3d019d9f7
dubilla <dan.ubilla@gmail.com>
2018-03-10 13:14:56 -0500

    Add seeds for testing library specs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Player.create(first_name: "Rickie", last_name: "Fowler", external_id: 3702)
+Player.create(first_name: "Adam", last_name: "Scott", external_id: 388)
