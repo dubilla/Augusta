8213d494cc1c5e78b5cb4fda76d49708ccbd7ac8
dubilla <dan.ubilla@gmail.com>
2018-03-25 18:16:20 -0400

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Augusta::Application.routes.draw do
 
   devise_for :users
 
-  root to: "home#index"
+  get 'welcome/index'
+
+  root to: "welcome#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
