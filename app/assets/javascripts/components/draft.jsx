c4240cb343a5496469960aa40b8d38c04635af6a
dubilla <dan.ubilla@gmail.com>
2019-02-12 22:00:48 -0500

    Continue translation from server-side draft to client-side. Show list of all golfers.

--- app/assets/javascripts/components/draft.jsx
+++ app/assets/javascripts/components/draft.jsx
@@ -1,5 +1,31 @@
+class AvailabilityTable extends React.Component {
+  render(props) {
+    const golfers = this.props.golfers;
+    const golferItems = golfers.map((golfer) =>
+      <tr key={golfer.id}>
+        <td>{golfer.first_name} {golfer.last_name}</td>
+      </tr>
+    );
+    return (
+      <table>
+        <tbody>
+          {golferItems}
+        </tbody>
+      </table>
+    );
+  }
+};
+
 class Draft extends React.Component {
-  render() {
-    return <h2>{this.props.league_name} Draft</h2>
+  render(props) {
+    return (
+      <div>
+        <h2>{this.props.league_name} Draft</h2>
+        <section>
+          <h3>Available Golfers</h3>
+          <AvailabilityTable golfers={this.props.golfers}/>
+        </section>
+      </div>
+    )
   }
 };
