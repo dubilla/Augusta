2be6328e46fdbc96007b24aad3a49bc53f1f7cd3
dubilla <dan.ubilla@gmail.com>
2020-12-01 21:40:19 -0500

    Add rails version to help with backwards compatability

--- db/migrate/20190512214105_add_constraint_to_completed_on_tournaments.rb
+++ db/migrate/20190512214105_add_constraint_to_completed_on_tournaments.rb
@@ -1,4 +1,4 @@
-class AddConstraintToCompletedOnTournaments < ActiveRecord::Migration
+class AddConstraintToCompletedOnTournaments < ActiveRecord::Migration[5.2]
   def change
     change_column_default :tournaments, :completed, false
   end
