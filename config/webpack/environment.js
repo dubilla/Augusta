eeb811ba717190f4ffb432730a88f350297c4ac1
dubilla <dan.ubilla@gmail.com>
2021-01-29 21:58:18 -0500

    Set up PostCSS Loader 3 for Tailwind compatability

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,17 +1,5 @@
 const { environment } = require('@rails/webpacker')
 
-// Hack for PostCSS 8 and Webpacker
-const css = environment.loaders.get('css');
-const sass = environment.loaders.get('sass');
-const postCssConfig = css.use.find(u => u.loader === 'postcss-loader');
-const postSassConfig = sass.use.find(u => u.loader === 'postcss-loader');
-if (postCssConfig) {
-  delete postCssConfig.options.config;
-}
-if (postSassConfig) {
-  delete postCssConfig.options.config;
-}
-
 const webpack = require('webpack');
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
