b6e377fead44379a6fba8ab8f466ac6f60570a6e
dubilla <dan.ubilla@gmail.com>
2021-01-31 17:04:34 -0500

    Remove unused leaderboard service to prepare to remove angular

--- app/javascript/src/leaderboard/leaderboard_service.js
+++
@@ -1,9 +0,0 @@
-angular.module('leaderboard')
-.factory('leadersService', ['$http', function($http) {
-  var service = {};
-
-  service.getAllLeaders = (leagueID, tournamentID) =>
-    $http.get('/leagues/' + leagueID + '/tournaments/' + tournamentID + '/leaderboard.json')
-
-  return service;
-}]);
